8f1396262ff0935bd599158636ad7979ae78d2a8
Ethan Nguyen <ethannguy2006@gmail.com>
2025-11-06 13:05:27 -0800

    string stuff

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 /Hi
 #include <iostream>
+#include <string>
 using namespace std;
 int main()
 {
     cout << "hello world" << endl;
     cout << "This is fun!" << endl;
+    
+    string s = "abc";
+    cout << s << endl;
 }
